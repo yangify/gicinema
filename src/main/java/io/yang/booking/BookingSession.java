a502a7bc8f23b3bbc65839d5697abf25e99388d8
hongyang <hongyangprivate@gmail.com>
2025-04-05 10:41:14 +0800

    chore: added documentation to BookingSession

--- src/main/java/io/yang/booking/BookingSession.java
+++ src/main/java/io/yang/booking/BookingSession.java
@@ -6,16 +6,33 @@ import io.yang.booking.display.Menu;
 
 import java.util.Scanner;
 
+/** Represents a booking session that continuously processes user input until it is closed. */
 public class BookingSession {
 
+  /** Indicates whether the session is still running. */
   private boolean isOpen;
+
+  /** Menu of available options for the user. */
   private final Menu menu;
 
+  /**
+   * Creates a new booking session with the provided options. By default, a newly created session is
+   * open.
+   *
+   * @param options an array of options used to build the session menu
+   */
   public BookingSession(Option[] options) {
     this.isOpen = true;
     this.menu = new Menu(options);
   }
 
+  /**
+   * Runs the main loop of the booking session. It displays the available options, gathers user
+   * input, and executes the corresponding action. The session continues until it is closed through
+   * an executed action.
+   *
+   * @param scanner the Scanner instance to read user input
+   */
   public void run(Scanner scanner) {
     while (isOpen) {
       menu.displayOptions();
