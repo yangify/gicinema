f53cc0d2f7822bb3d4f0b2dac42d76dfbfac066e
hongyang <hongyangprivate@gmail.com>
2025-04-06 11:35:59 +0800

    feat: added check booking behaviour

--- src/main/java/io/yang/booking/command/CheckBooking.java
+++ src/main/java/io/yang/booking/command/CheckBooking.java
@@ -1,12 +1,37 @@
 package io.yang.booking.command;
 
+import io.yang.cinema.Cinema;
+import io.yang.cinema.CinemaVisualizer;
+
+import java.util.Scanner;
+
 import static io.yang.printer.ConsolePrinter.printWriter;
 
 public class CheckBooking implements Action {
 
+  private final Cinema cinema;
+  private final Scanner scanner;
+
+  public CheckBooking(Cinema cinema, Scanner scanner) {
+    this.cinema = cinema;
+    this.scanner = scanner;
+  }
+
   @Override
   public void execute() {
-    printWriter.println("Checking bookings functionality executed.");
-    // TODO: Add check bookings logic here
+    String bookingId = solicitForBookingId();
+    if (bookingId.isEmpty()) return;
+    displayBookingDetails(bookingId);
+  }
+
+  private String solicitForBookingId() {
+    printWriter.println("Enter booking id, or enter blank to go back to main menu:");
+    return scanner.nextLine();
+  }
+
+  private void displayBookingDetails(String bookingId) {
+    printWriter.println("Booking id: " + bookingId);
+    CinemaVisualizer.visualize(cinema, bookingId);
+    printWriter.println();
   }
 }
--- src/main/java/io/yang/booking/option/CheckOption.java
+++ src/main/java/io/yang/booking/option/CheckOption.java
@@ -3,12 +3,11 @@ package io.yang.booking.option;
 import io.yang.booking.command.CheckBooking;
 import io.yang.cinema.Cinema;
 
+import java.util.Scanner;
+
 public class CheckOption extends Option {
 
-  private final Cinema cinema;
-
-  public CheckOption(Cinema cinema) {
-    super("Check bookings", new CheckBooking());
-    this.cinema = cinema;
+  public CheckOption(Cinema cinema, Scanner scanner) {
+    super("Check bookings", new CheckBooking(cinema, scanner));
   }
 }
